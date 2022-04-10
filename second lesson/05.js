463db5b38bb7bd990a554c3547e484ac1191a25d
Elvis <elvissamir18@gmail.com>
2022-04-10 19:02:16 -0400

    Change 05 and logger.js

--- second lesson/05.js
+++ second lesson/05.js
@@ -2,9 +2,15 @@ const EventEmitter = require('events')
 const emitter = new EventEmitter()
 
 // Register a listener
-emitter.on('messageLogged', () => {
-    console.log('Listener called')
+emitter.on('messageLogged', (args) => {
+    console.log('Listener called with args: ', args)
+})
+
+emitter.on('loggin', (data) => {
+    console.log('The message: ', data)
 })
 
 // Raise an event
-emitter.emit('messageLogged')
+emitter.emit('messageLogged', { id: 1, url: 'http' })
+
+module.exports = emitter
--- second lesson/logger.js
+++ second lesson/logger.js
@@ -1,8 +1,11 @@
 const url = 'http://mylogger.io/log'
+const emitter = require('./05')
 
 function log(message) {
-    console.log(message)
+    emitter.emit('loggin', message)
 }
+
+log("Hello World")
 
 module.exports.log = log
 module.exports.url = url
