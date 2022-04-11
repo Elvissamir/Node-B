c45bc674691ea1957c3e844ff438c4587b0bbc50
Elvis <elvissamir18@gmail.com>
2022-04-11 19:00:34 -0400

    Add additional notes on OS MODULE and code to 08

--- second lesson/08.js
+++ second lesson/08.js
@@ -4,6 +4,9 @@ const totalMemory = os.totalmem()
 const freeMemory = os.freemem()
 const arch = os.arch()
 
+console.log(os.cpus())
+console.log(os.platform())
+
 console.log(totalMemory / 1000000)
 console.log(freeMemory / 1000000)
 console.log(arch)
