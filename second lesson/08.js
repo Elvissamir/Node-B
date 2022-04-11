a1591d91d3454a9c4df08a6171a2640bb58b22a7
Elvis <elvissamir18@gmail.com>
2022-04-11 18:53:37 -0400

    Add 08 to second lesson

--- second lesson/08.js
+++ second lesson/08.js
@@ -1,1 +1,9 @@
-const os = require()
+const os = require('os')
+
+const totalMemory = os.totalmem()
+const freeMemory = os.freemem()
+const arch = os.arch()
+
+console.log(totalMemory / 1000000)
+console.log(freeMemory / 1000000)
+console.log(arch)
