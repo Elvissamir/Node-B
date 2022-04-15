cab9099953733893842b580650ab90a340247362
Elvis <elvissamir18@gmail.com>
2022-04-15 13:46:06 -0400

    Add http2m

---
+++ http2m.js
@@ -0,0 +1,12 @@
+const http2 = require('http2')
+const http = require('http')
+
+const server2 = http2.createSecureServer((req, res) => {
+
+})
+
+server2.listen(4000)
+
+const server = http.createServer()
+
+server.listen(3000)
